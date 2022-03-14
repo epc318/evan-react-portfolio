3ff6325286a39fe1e5263b4505ef0dfe21a221ca
Rick Johnson <carl4917@umn.edu>
2022-03-13 22:10:57 -0500

    creating simple footer and nav indexes

---
+++ src/component/footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function footer() {
+    return (
+        <footer>
+            <ul>
+                <li>GitHub Profile</li>
+                <li>LinkedIn</li>
+                <li>E-mail</li>
+            </ul>
+        </footer>
+    )
+}
+
+
+export default footer;
---
+++ src/component/nav/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function nav() {
+    return (
+        <header>
+            <h2>
+                <a href="/">Evan Carlson</a>
+            </h2>
+            <nav>
+                <ul>
+                    <li>Bio:</li>
+                    <li>Professional Resume</li>
+                    <li>Dev Portfolio</li>
+                    <li>Contact Information</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+export default nav;
