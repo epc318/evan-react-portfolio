1d18e7aeb0b6a0582982c5e5724eb10dd994444a
Rick Johnson <carl4917@umn.edu>
2022-03-13 23:36:18 -0500

    adding simple skillset/resume page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import nav from "./component/nav";
 import about from "./component/about";
 import portfolio from "./components/portfolio";
+import resume from "./component/resume";
 import footer from "./component/footer";
 
 
---
+++ src/component/resume/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+
+function resume() {
+    return (
+        <main>
+
+        <h1>Evan Carlson: Web Development Skillset:</h1>
+
+        <div>
+            <section>
+                <h2>Front End:</h2>
+                <ul>
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React.js</li>
+                    <li>jQuery</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Back End:</h2>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Sequelize.js</li>
+                    <li>Handlebars</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Technology/Development Software</h2>
+                <ul>
+                    <li>Git</li>
+                    <li>GitHub</li>
+                    <li>GitBash</li>
+                    <li>VSCode</li>
+                </ul>
+            </section>
+        </div>
+    </main>
+    )
+}
+
+
+export default resume;
