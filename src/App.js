4a7354f5e45a447e782f5dbea5cbd85e78a6a677
Rick Johnson <carl4917@umn.edu>
2022-03-13 22:00:49 -0500

    more setup, adding headshot, editing App.js to and linking future js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import nav from "./component/nav";
+import about from "./component/about";
+import footer from "./component/footer";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav></nav>
+
+      <about></about>
+
+      <footer></footer>
+    </>
   );
 }
-
-export default App;
